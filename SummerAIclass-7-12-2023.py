e796dc5b43384a2a4ab93ae9b261d4878c5c4d35
Ian D Thompson <iandasht@gmail.com>
2023-07-12 16:50:49 -0400

    class practice update

--- SummerAIclass-7-12-2023.py
+++ SummerAIclass-7-12-2023.py
@@ -1,8 +1,20 @@
-class Car:
-    def __init__(self, model, carbrand):
-        self.model = model
-        self.carbrand = carbrand
+class Faculty:
+    def __init__(self, name, tenure, salary):
+        self.name = name
+        self.tenure = tenure
+        self.salary = salary
 
-MyCar = Car("Elantra", "Hyundai")
+teacher1 = Faculty("Mr. Benedict", True, 1000000)
 
-print(f'My car is made by {MyCar.carbrand} and is an {MyCar.model}')
+if teacher1.tenure == True:
+    print(f'{teacher1.name} has tenure and makes ${teacher1.salary}')
+
+class Course:
+    def __init__(self, coursetitle, professors, dates):
+        self.coursetitle = coursetitle
+        self.professors = professors
+        self.dates = dates
+
+Class_AI = Course("Computational Account of AI", "Mr. Benedict", "Jul. 10-28")
+
+print(f'My class is called {Class_AI.coursetitle}, taught by {Class_AI.professors}. It will run from {Class_AI.dates}')
